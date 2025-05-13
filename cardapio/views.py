16dea701e3f5a5f7fba0ef129e4e2e0a84104591
Hugo Messias <hugomessias9@gmail.com>
2025-05-13 20:12:14 -0300

    feat: criado protótipo da página inicial do cardápio

--- cardapio/views.py
+++ cardapio/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views import View
+from django.http import HttpRequest
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Product, Menu
+
+
+class IndexView(View):
+    def get(self, request: HttpRequest):
+        menus = Menu.objects.all()
+        context = {
+            "menus": menus,
+        }
+        return render(request, "cardapio/index.html", context)
