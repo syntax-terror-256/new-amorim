9e1a4f8b9f383b58184b1bf9d964e118d0f95467
Hugo Messias <hugomessias9@gmail.com>
2025-06-24 09:52:20 -0300

    feat(backend): criado protótipo da popup de detalhes
    - Criado template 'cardapio/details.html'
    - Criado `DetailView` para entregar o template de detalhes
    - Atualizado lista `ALLOWED_HOSTS` para permitir que o servidor Django seja acessado por outros dispositivos na mesma rede

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-7k$u#k$vpo$pgt$!m1m819fgj!&x(@-$3w=++yt_u$-ukiym*l
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
